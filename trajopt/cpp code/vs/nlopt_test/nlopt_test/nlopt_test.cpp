66ad43ab61e5d965c079c97ffb5330ea6e290473
mbourgeois2 <mbourgeois2@umassd.edu>
2025-07-18 18:24:43 -0400

    add nlopt and switch to vscode

---
+++ trajopt/cpp code/vs/nlopt_test/nlopt_test/nlopt_test.cpp
@@ -0,0 +1,12 @@
+﻿// nlopt_test.cpp : Defines the entry point for the application.
+//
+
+#include "nlopt_test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
