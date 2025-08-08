23f847a5856f663ed48bd26512b90bb0767e8b9f
kindakool1 <76918064+kindakool1@users.noreply.github.com>
2025-08-07 21:33:15 -0400

    cleanup

--- trajopt/cpp code/rk4_dart_vsc/out/build/yes/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/xxkin/Documents/GitHub/mike/trajopt/cpp code/rk4_dart_vsc
-# Build directory: C:/Users/xxkin/Documents/GitHub/mike/trajopt/cpp code/rk4_dart_vsc/out/build/yes
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
